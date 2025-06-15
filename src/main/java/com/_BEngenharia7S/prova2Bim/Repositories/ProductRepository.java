c0d8087ba35a402987c84b0158b198c8a0cf6ddb
Hudson Matias <116506548+Hudson-Matias@users.noreply.github.com>
2025-06-15 14:34:58 -0300

    Adicionando os Repositories

---
+++ src/main/java/com/_BEngenharia7S/prova2Bim/Repositories/ProductRepository.java
@@ -0,0 +1,6 @@
+package com._BEngenharia7S.prova2Bim.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
---
+++ src/main/java/com/_BEngenharia7S/prova2Bim/Repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com._BEngenharia7S.prova2Bim.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
