d46ec3f4e0281f6f682444ed828bc79ff09e5b3b
Hudson Matias <116506548+Hudson-Matias@users.noreply.github.com>
2025-06-15 14:13:18 -0300

    Commitando as primeira etapas, criação do projeto!

---
+++ src/main/java/com/_BEngenharia7S/prova2Bim/Prova2BimApplication.java
@@ -0,0 +1,13 @@
+package com._BEngenharia7S.prova2Bim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova2BimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova2BimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_BEngenharia7S/prova2Bim/Prova2BimApplicationTests.java
@@ -0,0 +1,13 @@
+package com._BEngenharia7S.prova2Bim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova2BimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
